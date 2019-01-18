2134e15837c185c13e5639dbce9a3142dd1cc169
bcastet <benjamin.castet@etu.u-bordeaux.fr>
2019-01-18 15:00:02 +0100

    Score_reader.py

---
+++ Score_reader.py
@@ -0,0 +1,16 @@
+Nb_tests = 0.0
+Nb_success = 0.0
+with open('results/failed_stories.md') as f:
+    for line in f:
+        #print ("This line : "+line)
+        if line.startswith("    - utter_"):
+            if not(line.startswith("    - utter_salutation")):
+                if(len(line)<48):
+                    Nb_success+=1
+                else:
+                    print(line)
+
+        if line.startswith("## end-to-end story"):
+            Nb_tests+=1
+
+print("We have "+str(Nb_tests)+" tests and "+str(Nb_success)+" successes for a ratio of "+str(Nb_success/Nb_tests*100)+"%")
