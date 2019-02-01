95afb242412ccd0dc8251c3bcf1c38feeac59802
bcastet <benjamin.castet@etu.u-bordeaux.fr>
2019-02-01 15:50:08 +0100

    tests + scripts

--- Score_reader.py
+++ Score_reader.py
@@ -6,11 +6,16 @@ with open('results/failed_stories.md') as f:
         if line.startswith("    - utter_"):
             if not(line.startswith("    - utter_salutation")):
                 if(len(line)<48):
+                    print("Passed")
                     Nb_success+=1
                 else:
-                    print(line)
 
-        if line.startswith("## end-to-end story"):
-            Nb_tests+=1
+                    print(line)
+        else:
+            if line.startswith("## end-to-end story"):
+                Nb_tests+=1
+            else:
+                print(line)
+
 
 print("We have "+str(Nb_tests)+" tests and "+str(Nb_success)+" successes for a ratio of "+str(Nb_success/Nb_tests*100)+"%")
--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 # Entrainement 1
+python End2end_maker.py
 python -m rasa_core.evaluate default --core models/dialogue --nlu models/current/nlu --stories e2e_stories.md --e2e
+python Score_reader.py
