125c09449454d935e59235eccc6b695949a8e716
bcastet <benjamin.castet@etu.u-bordeaux.fr>
2019-04-09 16:43:34 +0200

    lanceur linux plus clean

--- generate_chatbots_linux.py
+++ generate_chatbots_linux.py
@@ -11,7 +11,7 @@ from subprocess import Popen
 
 # do other things here..
 # wait for completion
-
+port = 5005
 for file in dir:
 	if file.startswith("Chatbot"):
 		os.chdir(file)
@@ -23,5 +23,19 @@ for file in dir:
 		else:
 			print("Error while generating "+file)
 		os.system("chmod 777 chatbot.sh")
+		chatbotsh=open("chatbot.sh","w+")
+		chatbotsh.write(
+		"#!/bin/bash\n"+
+
+		"# Entrainement 1\n"+
+		"python3 -m rasa_core.train -d domain.yml -s Stories.md -o models/dialogue\n"+
+		"# Entrainement 2\n"+
+		"python3 -m rasa_nlu.train -c nlu_config.yml --data nlu.md -o models --fixed_model_name nlu --project current --verbose\n"+
+		"# Lancement du bot\n"+
+		"python3 -m rasa_core.run -d models/dialogue -u models/current/nlu -p "+str(port))
+		chatbotsh.close()
 		os.system("gnome-terminal -e ./chatbot.sh")
+
+		print("Launched "+str(file)+" on port "+str(port))
+		port+=1
 		os.chdir("..")
